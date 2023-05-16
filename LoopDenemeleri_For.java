e1280a3c2b0d98725b6918f3173c8a44570ab649
MEMKARAGOZ <130648118+MEMKARAGOZ@users.noreply.github.com>
2023-05-17 02:27:47 +0300

    LoopDenemeleri_For

---
+++ LoopDenemeleri_For.java
@@ -0,0 +1,23 @@
+public class LoopDenemeleri_For {
+    public static void main(String[] args) {
+        System.out.println("for döngüsü 1==>9");
+        for (int i =1; i<10; i++){
+            System.out.println(i);
+        }
+        System.out.println("for döngüsü 1==>10");
+
+        for (int i =1; i<=10; i++){
+            System.out.println(i);
+        }
+        System.out.println("for döngüsü tek sayilar");
+        for (int i =1; i<10; i+=2){
+            System.out.println(i);
+        }
+        System.out.println("for döngüsü çift sayilar");
+        for (int i =0; i<=10; i+=2){
+            System.out.println(i);
+        }
+        System.out.println("Döngü bitti :)");
+
+    }
+}
